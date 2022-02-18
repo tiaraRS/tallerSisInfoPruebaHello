ffe0f21bf96187cc9e7831fd049b0e336d7d3b05
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2022-02-17 20:46:47 -0400

    archivos iniciales p1

---
+++ P1_HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P1_HelloWorld
+{
+    [Route("api/helloWorld")]
+    public class HelloWorldController:Controller
+    {
+        [HttpGet]
+        public ActionResult<Object> Greet()
+        {
+            return Ok(new { message="Hello World" });
+        }
+    }
+}
---
+++ P1_HelloWorld/LambdaFunction.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P1_HelloWorld
+{
+    public class LambdaFunction 
+    {
+       
+    }
+}
